e645994c202c98c8ba2843e33cdabbf3cde7eb85
mantasmacionis <73352288+mantasmacionis@users.noreply.github.com>
2023-10-21 17:32:23 +0100

    deleted unused firstSem and i variables

--- Lab1/mutualEx.cpp
+++ Lab1/mutualEx.cpp
@@ -3,42 +3,37 @@
 #include <thread>
 #include <vector>
 
-
 static const int num_threads = 100;
-int sharedVariable=0;
-
+int sharedVariable = 0;
 
 /*! \fn updateTask
     \brief An Implementation of Mutual Exclusion using Semaphores
 
-   Uses C++11 features such as mutex and condition variables to implement an example of a rendezvous for threads
-
+    Uses C++11 features such as mutex and condition variables to implement an example of a rendezvous for threads
 */
-/*! displays a message that is split in to 2 sections to show how a rendezvous works*/
-void updateTask(std::shared_ptr<Semaphore> firstSem, int numUpdates){
-
- 
-  for(int i=0;i<numUpdates;i++){
-    //UPDATE SHARED VARIABLE HERE!
-    sharedVariable++;
-  }
-
+/*! displays a message that is split into 2 sections to show how a rendezvous works */
+void updateTask(std::shared_ptr<Semaphore> /* firstSem */, int numUpdates) {
+    for (int i = 0; i < numUpdates; i++) {
+        // UPDATE SHARED VARIABLE HERE!
+        sharedVariable++;
+    }
 }
 
+int main() {
+    std::vector<std::thread> vt(num_threads);
+    std::shared_ptr<Semaphore> aSemaphore(new Semaphore);
 
-int main(void){
-  std::vector<std::thread> vt(num_threads);
-  std::shared_ptr<Semaphore> aSemaphore( new Semaphore);
-  /**< Launch the threads  */
-  int i=0;
-  for(std::thread& t: vt){
-    t=std::thread(updateTask,aSemaphore,1000);
-  }
-  std::cout << "Launched from the main\n";
-  /**< Join the threads with the main thread */
-  for (auto& v :vt){
-      v.join();
-  }
-  std::cout << sharedVariable << std::endl;
-  return 0;
+    /**< Launch the threads  */
+    for (std::thread& t : vt) {
+        t = std::thread(updateTask, aSemaphore, 1000);
+    }
+    std::cout << "Launched from the main\n";
+
+    /**< Join the threads with the main thread */
+    for (auto& v : vt) {
+        v.join();
+    }
+
+    std::cout << sharedVariable << std::endl;
+    return 0;
 }
