c5d5575696290152ef15c96646f421f8986111f6
mantasmacionis <73352288+mantasmacionis@users.noreply.github.com>
2023-10-23 22:35:11 +0100

    Add files via upload

--- Lab3/Semaphore.cpp
+++ Lab3/Semaphore.cpp
@@ -1,25 +1,13 @@
 #include "Semaphore.h"
-/*! \class Semaphore
-    \brief A Semaphore Implementation
 
-   Uses C++11 features such as mutex and condition variables to implement Semaphore
-
-*/
-
-
-/*! closes thread */ 
-void Semaphore::Wait()
-{
-      std::unique_lock< std::mutex > lock(m_mutex);
-      m_condition.wait(lock,[&]()->bool{ return m_uiCount>0; });
-      --m_uiCount; 
+void Semaphore::Wait() {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    m_condition.wait(lock, [&]() -> bool { return m_uiCount > 0; });
+    --m_uiCount;
 }
 
-
-/*! allows a thread to open */
-void Semaphore::Signal() 
-{
-      std::unique_lock< std::mutex > lock(m_mutex);
-      ++m_uiCount;
-      m_condition.notify_one();
+void Semaphore::Signal() {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    ++m_uiCount;
+    m_condition.notify_one();
 }
