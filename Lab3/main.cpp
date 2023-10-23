a7934889456dc48ce3fe3fcef95f4f25756c4994
mantasmacionis <73352288+mantasmacionis@users.noreply.github.com>
2023-10-23 22:35:39 +0100

    Add files via upload

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,34 +1,30 @@
 #include "Barrier.h"
 #include <thread>
 #include <vector>
+#include <iostream>
 
-/*! displays the first function in the barrier being executed */
-void task(std::shared_ptr<Barrier> barrierObj){
+const int TotalThreads = 5;
 
-  std::cout << "first " << std::endl;
-  barrierObj->waitForAll();
-  std::cout << "second" << std::endl;
+void task(std::shared_ptr<Barrier> barrierObj) {
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "first " << std::endl;
+        barrierObj->waitForAll();
+        std::cout << "second" << std::endl;
+        barrierObj->waitForAll();
+    }
 }
 
+int main(void) {
+    std::vector<std::thread> threadArray(TotalThreads);
+    std::shared_ptr<Barrier> barrierObj(new Barrier(5));
 
+    for (int i = 0; i < threadArray.size(); ++i) {
+        threadArray[i] = std::thread(task, barrierObj);
+    }
 
+    for (int i = 0; i < threadArray.size(); i++) {
+        threadArray[i].join();
+    }
 
-int main(void){
-
-  /*!< An array of threads*/
-  std::vector<std::thread> threadArray(5);
-  /*!< Pointer to barrier object*/
-  std::shared_ptr<Barrier> barrierObj( new Barrier);
-
-  barrierObj->setCount(5);
-
-  for(int i=0; i < threadArray.size(); i++){
-    threadArray[i]=std::thread(task,barrierObj);
-  }
-
-  for(int i = 0; i < threadArray.size(); i++){
-    threadArray[i].join();
-  }
-  
-  return 0;
+    return 0;
 }
